ea2c590604ddc639613b72a8b52eab20edf95df3
vagrant <kevin.fu842@gmail.com>
2020-09-27 23:20:08 +0000

    exercise 2.1 done

--- exercise_2_1/exercise_2_1.cpp
+++ exercise_2_1/exercise_2_1.cpp
@@ -1,5 +1,36 @@
+#include <iostream>
 #include <vector>
+#include "convolution.hpp"
 
 int main() {
+    std::vector<double> x;
+    std::vector<double> w;
+    std::vector<double> y;
+    std::vector<double> y2;
+    bool pack_with_zeros = true;
 
+    std::string s;
+    std::cin >> s;
+    if(s == "false") {
+        pack_with_zeros = false;
+    }
+    std::cin >> s;
+    x = readInVector(s);
+    std::cin >> s;
+    w = readInVector(s);
+
+    // TODO write your code here
+    // =========== START =========
+    y = applyConvolution(x, w, true);
+    y2 = applyConvolution(x, w, false);
+
+    // printVector("x", x);
+    // printVector("w", w);
+
+    printVector(y);
+    printVector(y2);
+
+    // =========== END ===========
+
+    return 0;
 }
