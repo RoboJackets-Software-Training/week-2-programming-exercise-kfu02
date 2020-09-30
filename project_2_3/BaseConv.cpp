bc6f4a9bd2de3b6651f14aec30b3489d8d10c0d7
vagrant <kevin.fu842@gmail.com>
2020-09-30 02:21:46 +0000

    finished 2_3

---
+++ project_2_3/BaseConv.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include "BaseConv.hpp"
+
+BaseConv::BaseConv(std::vector<int> const &inStats){
+  stats = inStats;
+}
+
+BaseConv::~BaseConv() {
+  printStats();
+  stats.clear();
+}
+
+int BaseConv::charToInt(char c) {
+  if (c > 57) { // hex chars
+    return c - (65 - 10);
+  } else {
+    return c - 48;
+  }
+}
+
+char BaseConv::intToChar(int i) {
+  if (i < 10) {
+    return i + 48;
+  } else { // hex chars
+    return (i - 10) + 65; // given code was wrong
+  }
+}
+
+int BaseConv::binToDec(std::string bin) {
+  int dec = 0;
+  for(int i = 0; i < bin.length(); i++){
+    int digit = charToInt(bin[bin.length()-1-i]);
+    dec += digit * std::pow(2, i);
+  }
+  return dec;
+}
+
+int BaseConv::hexToDec(std::string hex) {
+  int dec = 0;
+  for(int i = 0; i < hex.length(); i++){
+    int digit = charToInt(hex[hex.length()-1-i]);
+    dec += digit * std::pow(16, i);
+  }
+  return dec;
+}
+
+std::string BaseConv::decToHex(std::string dec) {
+  std::string hex = "";
+  int rem = std::stoi(dec);
+  while (rem >= 16) {
+    hex += intToChar(rem % 16);
+    rem /= 16;
+  }
+  hex += intToChar(rem % 16);
+
+  std::reverse(hex.begin(), hex.end());
+  return hex;
+}
+
+std::string BaseConv::binToHex(std::string bin) {
+  std::string hex = "";
+  std::string block = "";
+  int rem = bin.length() % 4;
+
+  for (int i = bin.length()-4; i >= rem; i -= 4) {
+    block = bin.substr(i, 4);
+    hex += intToChar(binToDec(block));
+  }
+  if (rem != 0) {
+    block = bin.substr(0, rem);
+    hex += intToChar(binToDec(block));
+  }
+
+  std::reverse(hex.begin(), hex.end());
+  return hex;
+}
+
+std::string BaseConv::hexToBin(std::string hex) {
+  return decToBin(std::to_string(hexToDec(hex)));
+}
+
+std::string BaseConv::decToBin(std::string dec) {
+  std::string bin = "";
+  int rem = std::stoi(dec);
+  while (rem >= 2) {
+    bin += intToChar(rem % 2);
+    rem /= 2;
+  }
+  bin += intToChar(rem % 2);
+
+  std::reverse(bin.begin(), bin.end());
+  return bin;
+}
+
+void BaseConv::addIOStats(std::string inType, std::string outType) {
+  if (inType == "d") {
+    stats[0] += 1;
+  } else if (inType == "b") {
+    stats[1] += 1;
+  } else if (inType == "h") {
+    stats[2] += 1;
+  }
+
+  if (outType == "d") {
+    stats[3] += 1;
+  } else if (outType == "b") {
+    stats[4] += 1;
+  } else if (outType == "h") {
+    stats[5] += 1;
+  }
+}
+
+void BaseConv::printStats() {
+  std::cout << "decimal_inputs: " << stats[0] << std::endl;
+  std::cout << "binary_inputs: " << stats[1] << std::endl;
+  std::cout << "hex_inputs: " << stats[2] << std::endl;
+  std::cout << "decimal_outputs: " << stats[3] << std::endl;
+  std::cout << "binary_outputs: " << stats[4] << std::endl;
+  std::cout << "hex_outputs: " << stats[5] << std::endl;
+}
